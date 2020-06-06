f794c4d1e533319949d2be98b2304fc182391f1c
Adona Tesfaye <adonatesfaye99@gmail.com>
2020-06-06 17:27:55 +0300

    Updates

--- src/components/search-Box/search-Box.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import './search-box.styles.css'
-
-
-export const SearchBox=(props)=>(
-    <input className="search" onChange={props.handleChange} type="search" placeholder={props.placeholder}/>
-)
